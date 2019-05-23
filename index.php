cfbf102a881e33bb65fb0b6f1e700a3631126b59
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2019-05-23 11:55:35 +0200

    Wersja startowa

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__ . '/php/init.php'; ?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <?php require_once 'section/head.php'; ?>
+</head>
+<body>
+    <?php require_once 'layout/content.php'; ?>
+    <?php require_once 'section/body.php'; ?>
+</body>
+</html>
---
+++ section/head.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="module/fontawesome/css/all.css">
+<link rel="stylesheet" href="app/composer/vendor/twbs/bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" href="layout/style/main.css">
+<title>aqua.net.pl</title>
