4b21cd4c526709fa24011028ae6deb3e39c2fd34
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2019-07-01 19:31:45 +0200

    Slider with label, no rocket, spinning logo

--- layout/content/slider.php
+++ layout/content/slider.php
@@ -1,10 +1,11 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-xs">
+        <div class="col-12" style="padding: 0">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <?php
                     $slider = scandir('layout/graphic/slider');
+                    $countRowDataDisplay = 0;
                     $active = ' active';
                     foreach ($slider as $s) {
 
@@ -20,6 +21,8 @@
                         if($active != '')
                             $active = '';
 
+                        $countRowDataDisplay++;
+
                     }
                     
                     ?>
@@ -30,6 +33,20 @@
                 <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                     <i class="fal fa-chevron-circle-right"></i>
                 </a>
+                <?php
+
+                $active = ' class="active"';
+                echo '<ol class="carousel-indicators">';
+                for ($i = 0; $i < $countRowDataDisplay; $i++) {
+
+                    echo '<li data-target="#carouselExampleControls" data-slide-to="'.$i.'"'.$active.'></li>';
+
+                    $active = '';
+
+                }
+                echo '</ol>';
+
+                ?>
             </div>
         </div>
     </div>
