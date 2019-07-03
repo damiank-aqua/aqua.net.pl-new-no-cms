bf5d1a40c473c8bc285347e9001b94d52e89a7d7
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2019-07-03 15:38:38 +0200

    Format text content

--- data/dla-instalatorow/nawodnienia/informacja.php
+++ data/dla-instalatorow/nawodnienia/informacja.php
@@ -1,3 +1,6 @@
-<h3>Witamy bardzo serdecznie na stronie naszej firmy.</h3>
-<p>Znajdujecie się Państwo w dziale poświęconym obsłudze instalatorów nawodnieniowych. Nasza firma od ponad 15 lat oferuje najwyższej jakości rozwiązania z zakresu automatycznych systemów nawadniających. Jesteśmy w Polsce jedną z pierwszych firm działającą w tym segmencie. Dzięki siec 16 hurtowni i 60 handlowców mamy możliwość obsłużenia w regionie niemalże każdej inwestycji. W naszej ofercie znajdziecie Państwo takich producentów jak Hunter, Rain Bird, Rain SPS czy Toro.  Bardzo dobrze rozumiemy, że instalator nawodnieniowy to odrębna grupa klientów wymagająca kompleksowej oferty i obsługi o czym można się przekonać w naszych jednostkach.</p>
-<p><em>Serdecznie zapraszamy do współpracy i naszych oddziałów.</em></p>
+<h3>Witamy bardzo serdecznie na stronie naszej firmy</h3>
+<img src="layout/graphic/3.jpg" style="margin: 15px; float: left;">
+<p style="line-height: 26px; padding-top: 10px; font-size: 14px">
+    Znajdujecie się Państwo w dziale poświęconym obsłudze instalatorów nawodnieniowych. Nasza firma <strong>od ponad 15 lat</strong> oferuje najwyższej jakości rozwiązania z zakresu automatycznych systemów nawadniających. Jesteśmy w Polsce jedną z pierwszych firm działającą w tym segmencie. <strong>Dzięki siec 16 hurtowni i 60 handlowców</strong> mamy możliwość obsłużenia w regionie niemalże każdej inwestycji. W naszej ofercie znajdziecie Państwo takich producentów jak <strong>Hunter, Rain Bird, Rain SPS czy Toro</strong>.  Bardzo dobrze rozumiemy, że instalator nawodnieniowy to odrębna grupa klientów wymagająca kompleksowej oferty i obsługi o czym można się przekonać w naszych jednostkach.</p>
+<hr><p><em style="font-size: 15px">Serdecznie zapraszamy do współpracy i naszych oddziałów.</em></p>
+<br><br><br><br>
